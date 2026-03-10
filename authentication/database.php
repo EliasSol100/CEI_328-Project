572aed7cdfda8051d797e1cae2c8d3cc2f84fe87
Christina Antoniou <chrisanton1705@gmail.com>
2026-03-10 17:32:18 +0200

    something was wrong with the connection fixed it

--- authentication/database.php
+++ authentication/database.php
@@ -1,14 +1,19 @@
 ﻿<?php
+// Enable detailed error reporting
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$hostName = "localhost";
+$hostName = "127.0.0.1";   // Use IP instead of localhost
 $dbUser = "root";
-$dbPassword = "";
+$dbPassword = "";          // No password
 $dbName = "athina_eshop";
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
- 
+$port = 3307;              /
+
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, $port);
+
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
+// Set charset to utf8mb4
+mysqli_set_charset($conn, "utf8mb4");
 ?>
-
