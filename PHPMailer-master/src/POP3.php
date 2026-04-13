e6a938a1385b2dfef74993addb8b4c619e7d0dd1
Elias <eliassolomonides0@gmail.com>
2026-04-13 20:00:00 +0300

    Upgrade PHPMailer to 7.0.2
    Update the bundled PHPMailer copy to 7.0.2, including upstream documentation, language files, and transport classes.
    
    This keeps the project on the latest library version while preserving compatibility with the existing direct SMTP setup.

--- PHPMailer-master/src/POP3.php
+++ PHPMailer-master/src/POP3.php
@@ -45,8 +45,9 @@ class POP3
      * The POP3 PHPMailer Version number.
      *
      * @var string
+     * @deprecated This constant will be removed in PHPMailer 8.0. Use `PHPMailer::VERSION` instead.
      */
-    const VERSION = '6.9.1';
+    const VERSION = '7.0.2';
 
     /**
      * Default POP3 port number.
@@ -250,7 +251,9 @@ class POP3
 
         //On Windows this will raise a PHP Warning error if the hostname doesn't exist.
         //Rather than suppress it with @fsockopen, capture it cleanly instead
-        set_error_handler([$this, 'catchWarning']);
+        set_error_handler(function () {
+            call_user_func_array([$this, 'catchWarning'], func_get_args());
+        });
 
         if (false === $port) {
             $port = static::DEFAULT_PORT;
